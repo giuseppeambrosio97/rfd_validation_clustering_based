9dc21396c1df0a74e7e6c4f92faca2c21bc5a8be
Giuseppe Ambrosio <72221445+giuseppeambrosio97@users.noreply.github.com>
2021-10-13 23:42:28 +0200

    Delete clustering_based_rfd_validation-master directory

--- clustering_based_rfd_validation-master/lib/utils.py
+++
@@ -1,7 +0,0 @@
-def dist_int_abs(a: int, b: int):
-    return abs(a - b)
-
-
-def stream_generator(dataset, chunk_size):
-    for i in range(0, len(dataset), chunk_size):
-        yield dataset[i:i + chunk_size]
